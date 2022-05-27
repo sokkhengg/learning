5b421f2fcbb53ec36af679fc72fbccf9321cfe4a
Sokheng Teang <93559386+sokkhengg@users.noreply.github.com>
2022-05-27 07:30:53 -0700

    anagram first solution

--- JavaScript/frequencyCounterPattern.js
+++ JavaScript/frequencyCounterPattern.js
@@ -43,3 +43,28 @@ function same(arr1, arr2){
 }
 
 // ([1,2,3,2,5], [9,1,4,4,11])
+
+// valid anagram
+
+function validAnagram(word1, word2) {
+    let counter = {}
+
+    if (word1.length !== word2.length) {
+        return false
+    } else {
+        for (let char of word1) {
+            counter[char] ? (counter[char] += 1) : (counter[char] = 1)
+        }
+
+        for (let char of word2) {
+            counter[char] ? (counter[char] -= 1) : false
+        }
+
+        for (let key in counter) {
+            if (counter[key] !== 0) {
+                return false
+            }
+        }
+     }
+     return true
+}
